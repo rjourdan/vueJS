9c1cdedbcd0a34341e250a231141f6b6658b2751
Romain Jourdan <rjourdan@C02VX5SSHTD6-MAC.local>
2018-11-29 17:03:54 -0800

    Getting excited!!!!!

---
+++ lesson 14 - conditional outputs/app.js
@@ -0,0 +1,12 @@
+new Vue({
+   el: '#app', 
+   data: {
+       title: 'Becoming a Vue Ninja',
+       name: 'Ryu'
+   },
+   methods: {
+    logMessage(){
+        console.log('hello world')
+    }
+   }
+})
