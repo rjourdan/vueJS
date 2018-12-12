5cabaa2497aae450ed2e5619c63a27f8083e0c99
Romain Jourdan <rjourdan@C02VX5SSHTD6-MAC.local>
2018-12-12 05:57:03 -0800

    Ninja-chat

---
+++ ninja-chat/src/firebase/init.js
@@ -0,0 +1,23 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyD2qBQKQERj8HTddUpJNCnUSUP7nuntv-g",
+    authDomain: "ninja-chat-1c89b.firebaseapp.com",
+    databaseURL: "https://ninja-chat-1c89b.firebaseio.com",
+    projectId: "ninja-chat-1c89b",
+    storageBucket: "ninja-chat-1c89b.appspot.com",
+    messagingSenderId: "585155443997"
+  };
+  firebase.initializeApp(config);
+</script>
+
+const firebaseApp = firebase.initializeApp(config);
+  firebaseApp.firestore().settings( { timestampsInSnapshots: true})
+
+  //export firestore database
+  export const db = firebaseApp.firestore()
+  export const auth = firebaseApp.auth()
